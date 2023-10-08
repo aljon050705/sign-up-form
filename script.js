6a60fb27d80fd5a82bf8dd1c509db4af2076dcb5
Aljon Valdez <aljonvaldez2005@gmail.com>
2023-10-09 00:37:04 +0400

    design format done and functional

---
+++ script.js
@@ -0,0 +1,18 @@
+password = document.getElementById("password")
+confirmpassword = document.getElementById("confirmpassword")
+error = document.getElementById("error")
+
+function validatePassword() {
+    console.log(confirmpassword.value)
+    if (password.value != confirmpassword.value) {
+        error.textContent = "Password doesn't match";
+        confirmpassword.classList.add("invalid");
+    } else {
+        error.textContent = "";
+        confirmpassword.classList.remove("invalid");
+    }
+}
+
+password.addEventListener("input", validatePassword);
+confirmpassword.addEventListener("input", validatePassword);
+
